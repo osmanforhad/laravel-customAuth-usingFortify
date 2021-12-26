054a491361df0c5336048dc213d7a1f3cbf5686c
osmanforhad <osmanforhad93@gmail.com>
2021-12-26 09:48:57 +0600

    setup app layout and design login register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,26 @@
-login file
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-4"></div>
+    <div class="card p-4 mt-3 col-4">
+        <h4>Login Your Account</h4>
+        <form method="POST" action="#">
+            @csrf
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">LOGIN</button>
+        </form>
+    </div>
+</div>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,30 @@
-this is register
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-4"></div>
+    <div class="card p-4 mt-3 col-4">
+        <h4>Register a new account</h4>
+        <form method="POST" action="#">
+            @csrf
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Name</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" name="password_confirm">
+            </div>
+            <button type="submit" class="btn btn-primary">REGISTER</button>
+        </form>
+    </div>
+</div>
+@endsection
